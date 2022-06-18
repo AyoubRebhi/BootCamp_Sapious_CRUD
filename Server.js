9d7496343e94cdc5a374ae8a22db78850447ea47
ayoub rebhi <ayoubrebhi1230@gmail.com>
2022-06-18 14:30:58 +0200

    2nd push

---
+++ Server.js
@@ -0,0 +1,48 @@
+//import section
+const express= require("express");
+const { default: mongoose } = require("mongoose");
+const app=express();
+const mongoose =require("mongoose");
+
+const Stagiere = require("./models/stagiere")
+
+app.get('/stagieres',(req,res)=>{
+    
+    try {
+        await Stagiere.find({ })
+        .then( resultat => {
+            res.send(resultat)
+        })
+        
+    } catch (error) {
+        console.log(err)
+    }
+   
+});
+
+
+//db connection
+mongoose.connect(
+    "mongodb+srv://youbi2012M:123456789Bestfriend@cluster0.4yv5m.mongodb.net/?retryWrites=true&w=majority"
+);
+//ki yabda connecté ykharrej msg li howa connecté 
+mongoose.connection.on("connected", ()=>{
+    console.log("DB connected");
+});
+// ki tabda famma error ykharrajlek 
+mongoose.connection.on("error", (err)=>{
+    console.log("mongodb failed with ", err);
+});
+
+
+//middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: false}));
+//middleware rootes
+
+//listening server
+const port=5500;
+
+app.listen(port, ()=>{
+    console.log(`Example app listening on port ${port}`);
+});
---
+++ models/stagiere.js
@@ -0,0 +1,31 @@
+const mongoose= require("mongoose");
+
+const stagiereSchema=mongoose.Schema({
+    cin:{
+        type: Number,
+        required:true,
+    },
+    nom:{
+        type: String,
+        required:true,
+    },
+    prenom:{
+        type: String,
+        required:true,
+    },
+    email:{
+        type: String,
+        required:true,
+    },
+    encadreur:{
+        type: String,
+        required: true,
+    },
+    departement:{
+        type: String,
+        required: true,
+    },
+
+});
+
+module.exports =Stagiere = mongoose.model("stagiere ", stagiereSchema);
